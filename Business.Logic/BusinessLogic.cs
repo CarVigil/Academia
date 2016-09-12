44aac987cf23cf58fc5251c8a823d844d1c7e51b
Carla <carvigil21@gmail.com>
2016-09-12 20:35:13 -0300

    Add files via upload

---
+++ Business.Logic/BusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class BusinessLogic 
+    {
+        Data.Database.UsuarioAdapter _UsuarioData;
+        Data.Database.PersonaAdapter _PersonaData;
+        Data.Database.PlanAdapter _PlanData;
+    }
+}
---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+
+        Data.Database.UsuarioAdapter _UsuarioData = new Data.Database.UsuarioAdapter();
+
+        public UsuarioLogic()
+        {
+           this.UsuarioData = null;
+        }
+          
+        public Data.Database.UsuarioAdapter UsuarioData
+        {
+            get { return _UsuarioData; }
+            set { _UsuarioData = value; }
+        }
+
+        public List<Usuario> GetAll ()
+        {
+            return UsuarioData.GetAll();
+        }
+        
+        public Usuario GetOne (int  Id )
+        {
+            return UsuarioData.GetOne(Id);
+        }
+
+        public void Delete(int Id)
+        {
+            UsuarioData.Delete(Id);
+        }
+
+        public void Save(Business.Entities.Usuario Usuario)
+        {
+            UsuarioData.Save(Usuario);
+        }
+
+    }
+}
